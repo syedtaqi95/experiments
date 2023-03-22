58592d0860fd6b0e6c4854a78710c74825ec933e
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-03-22 11:23:13 +0000

    Added transpilePackages for bit packages to next.config.js to transpile ES6 modules

--- bit/use-hello-world/next.config.js
+++ bit/use-hello-world/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@syedtaqi95/personal.ui.hello-world', '@syedtaqi95/personal.get-hello-world']
 }
 
 module.exports = nextConfig
