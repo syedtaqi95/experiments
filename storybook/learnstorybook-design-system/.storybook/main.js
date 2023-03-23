e2d2f3dead5e561fd67731584c770506f1057710
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-03-23 11:39:43 +0000

    Built UI Components and added Storybook controls and actions addons

--- storybook/learnstorybook-design-system/.storybook/main.js
+++ storybook/learnstorybook-design-system/.storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
   ],
   framework: "@storybook/react",
 };
