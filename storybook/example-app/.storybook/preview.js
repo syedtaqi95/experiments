a59c988f0ebcc6beb0b108e4535fbb5ec7d7a7f8
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-03-24 11:02:27 +0000

    Imported Avatar from library into UserItem component

--- storybook/example-app/.storybook/preview.js
+++ storybook/example-app/.storybook/preview.js
@@ -1,3 +1,27 @@
+import React from "react";
+
+// The styles imported from the design system.
+import { global as designSystemGlobal } from "@syedtaqi95/learnstorybook-design-system";
+
+const { GlobalStyle } = designSystemGlobal;
+
+/*
+ * Adds a global decorator to include the imported styles from the design system.
+ * More on Storybook decorators at:
+ * https://storybook.js.org/docs/react/writing-stories/decorators#global-decorators
+ */
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+/*
+ * More on Storybook parameters at:
+ * https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+ */
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
