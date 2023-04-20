6a2999af43928945c160db0362d26132820d66bf
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-04-20 16:13:13 +0100

    Added context menu in pyqt app

--- pyqt/pyqt/app.py
+++ pyqt/pyqt/app.py
@@ -2,48 +2,20 @@
 import sys
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QTextEdit
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QMenu
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.label = QLabel("Click in this window")
-        self.setCentralWidget(self.label)
-
-        self.setMouseTracking(True)
-
-    def mouseMoveEvent(self, e) -> None:
-        self.label.setText("mouseMoveEvent")
-
-    def mousePressEvent(self, e) -> None:
-        if e.button() == Qt.MouseButton.LeftButton:
-            self.label.setText("mousePressEvent LEFT")
-        elif e.button() == Qt.MouseButton.RightButton:
-            self.label.setText("mousePressEvent RIGHT")
-        elif e.button() == Qt.MouseButton.MiddleButton:
-            self.label.setText("mousePressEvent MIDDLE")
-
-    def mouseReleaseEvent(self, e) -> None:
-        if e.button() == Qt.MouseButton.LeftButton:
-            self.label.setText("mouseReleaseEvent LEFT")
-
-        elif e.button() == Qt.MouseButton.MiddleButton:
-            self.label.setText("mouseReleaseEvent MIDDLE")
-
-        elif e.button() == Qt.MouseButton.RightButton:
-            self.label.setText("mouseReleaseEvent RIGHT")
-
-    def mouseDoubleClickEvent(self, e) -> None:
-        if e.button() == Qt.MouseButton.LeftButton:
-            self.label.setText("mouseDoubleClickEvent LEFT")
-
-        elif e.button() == Qt.MouseButton.MiddleButton:
-            self.label.setText("mouseDoubleClickEvent MIDDLE")
-
-        elif e.button() == Qt.MouseButton.RightButton:
-            self.label.setText("mouseDoubleClickEvent RIGHT")
+    def contextMenuEvent(self, e) -> None:
+        context = QMenu(self)
+        context.addAction(QAction("test 1", self))
+        context.addAction(QAction("test 2", self))
+        context.addAction(QAction("test 3", self))
+        context.exec(e.globalPos())
 
 
 def main():
