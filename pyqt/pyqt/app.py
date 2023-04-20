95195b636704d7060489a4e21ff853718ef3edcb
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-04-20 15:27:47 +0100

    Added signals to pyqt app

--- pyqt/pyqt/app.py
+++ pyqt/pyqt/app.py
@@ -1,38 +1,46 @@
 # Only needed for access to command line arguments
 import sys
+from random import choice
 
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
+    QWidget,
+)
 
 
-# Subclass QMainWindow to customise your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("My App")
-        button = QPushButton("Press Me")
-        self.setFixedSize(QSize(400, 300))
+        self.setFixedSize(400, 300)
 
-        # Set the central widget of the Window
-        self.setCentralWidget(button)
+        self.label = QLabel()
+
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
 
 
 def main():
-    # You need one (and only one) QApplication instance per application.
-    # Pass in sys.argv to allow command line arguments for your app.
-    # If you know you won't use command line arguments QApplication([]) works too.
     app = QApplication(sys.argv)
 
-    # Create a Qt widget, which will be our window.
     window = MainWindow()
-    window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+    window.show()
 
-    # Start the event loop.
     app.exec()
-
-    # Your application won't reach here until you exit and the event
-    # loop has stopped.
 
 
 if __name__ == "__main__":
