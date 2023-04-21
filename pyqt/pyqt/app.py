fe011d5a770d38584f81a9d9557b6df8b8114333
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-04-21 12:16:05 +0100

    Added QLayouts

--- pyqt/pyqt/app.py
+++ pyqt/pyqt/app.py
@@ -1,41 +1,47 @@
 # Only needed for access to command line arguments
-import sys, os
-from typing import Type
-
+import sys
+from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget, QHBoxLayout, QVBoxLayout
+from PyQt6.QtGui import QPalette, QColor
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import (
-    QMainWindow,
-    QApplication,
-    QLabel,
-    QCheckBox,
-    QComboBox,
-    QListWidget,
-    QLineEdit,
-    QLineEdit,
-    QSpinBox,
-    QDoubleSpinBox,
-    QSlider,
-)
-from PyQt6.QtGui import QPixmap
+
+
+# Qt.GlobalColor._member_names_
+class Color(QWidget):
+    def __init__(self, color: Qt.GlobalColor | str) -> None:
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.setWindowTitle("Widgets App")
+        self.setWindowTitle("My App")
 
-        widget = QLabel()
-        widget.setPixmap(
-            QPixmap(
-                os.path.join(
-                    "/", os.path.dirname(os.path.realpath(__file__)), "test-image.png"
-                )
-            )
-        )
-        widget.setScaledContents(True)
-        widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout1 = QHBoxLayout()
+        layout2 = QVBoxLayout()
+        layout3 = QVBoxLayout()
 
+        layout2.addWidget(Color("red"))
+        layout2.addWidget(Color("yellow"))
+        layout2.addWidget(Color("purple"))
+        layout1.addLayout(layout2)
+        
+        layout1.addWidget(Color("green"))
+
+        layout3.addWidget(Color("red"))
+        layout3.addWidget(Color("purple"))
+        layout1.addLayout(layout3)
+
+        layout1.setContentsMargins(0, 0, 0, 0)
+        layout1.setSpacing(20)
+
+        widget = QWidget()
+        widget.setLayout(layout1)
         self.setCentralWidget(widget)
 
 
