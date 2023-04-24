43f6eef575178626cf54e120cc7cccfd38c46b9a
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-04-24 15:01:36 +0100

    Created toolbar with PySide6

--- python-qt/python-qt/app.py
+++ python-qt/python-qt/app.py
@@ -1,11 +1,10 @@
-# Only needed for access to command line arguments
 import sys
 from os import path
 from typing import Type
 
-from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QToolBar, QStatusBar
+from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QToolBar, QStatusBar, QCheckBox
 from PySide6.QtGui import QAction, QIcon
-from PySide6.QtCore import Qt, Slot
+from PySide6.QtCore import Qt, Slot, QSize
 
 
 class MainWindow(QMainWindow):
@@ -13,6 +12,43 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("My App")
+
+        label = QLabel("Hello")
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.setCentralWidget(label)
+
+        toolbar = QToolBar("Main toolbar")
+        toolbar.setIconSize(QSize(16, 16))
+        self.addToolBar(toolbar)
+
+        currentDirectory = path.dirname(path.realpath(__file__))
+
+        buttonAction = QAction(
+            QIcon(path.join("/", currentDirectory, "bug.png")), "The Button", self
+        )
+        buttonAction.setStatusTip("This is The Button")
+        buttonAction.triggered.connect(self.onToolbarButtonClick)
+        buttonAction.setCheckable(True)
+        toolbar.addAction(buttonAction)
+
+        toolbar.addSeparator()
+
+        buttonAction2 = QAction(
+            QIcon(path.join("/", currentDirectory, "bug.png")), "Button 2", self
+        )
+        buttonAction2.setStatusTip("This is Button 2")
+        buttonAction2.triggered.connect(self.onToolbarButtonClick)
+        buttonAction2.setCheckable(True)
+        toolbar.addAction(buttonAction2)
+
+        toolbar.addWidget(QLabel("The Label"))
+        toolbar.addWidget(QCheckBox())
+
+        self.setStatusBar(QStatusBar(self))
+
+    def onToolbarButtonClick(self, s: Type[Slot]):
+        print(f"click {s}")
 
 
 def main():
