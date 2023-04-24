b5d1cfb964ae23723d68736b5c27a32447d8c088
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-04-24 16:20:50 +0100

    Created new window

--- python-qt/python-qt/app.py
+++ python-qt/python-qt/app.py
@@ -1,57 +1,48 @@
 import sys
-from typing import Type, Optional
+from typing import Type
+from random import randint
 
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow,
     QLabel,
-    QDialog,
-    QDialogButtonBox,
     QPushButton,
     QVBoxLayout,
     QWidget,
-    QMessageBox,
 )
 from PySide6.QtCore import Slot
 
 
-class CustomDialog(QDialog):
-    def __init__(self, parent: Optional[QWidget] = None):
-        super().__init__(parent)
-        self.setWindowTitle("My Dialog")
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window as we want.
+    """
 
-        Qbtn = (
-            QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
-        )
-
-        self.buttonBox = QDialogButtonBox(Qbtn)
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
-
-        self.layout = QVBoxLayout()  # type: ignore
-        message = QLabel("Something happened, is that OK?")
-        self.layout.addWidget(message)
-        self.layout.addWidget(self.buttonBox)
-        self.setLayout(self.layout)
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Another Window % d" % randint(0, 100))
+        layout.addWidget(self.label)
+        self.setLayout(layout)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.w = None  # No external window yet.
+        self.button = QPushButton("Push for Window")
+        self.button.clicked.connect(self.show_new_window)
+        self.setCentralWidget(self.button)
 
-        self.setWindowTitle("My App")
+    def show_new_window(self, checked: Type[Slot]):
+        if self.w is None:
+            self.w = AnotherWindow()
+            self.w.show()
 
-        button = QPushButton("Press me for a dialog!")
-        button.clicked.connect(self.button_clicked)
-        self.setCentralWidget(button)
-
-    def button_clicked(self, s: Type[Slot]):
-        button = QMessageBox.question(self, "Question dialog", "The longer message")
-
-        if button == QMessageBox.StandardButton.Yes:
-            print("Yes!")
         else:
-            print("No!")
+            self.w.close()  # Close window.
+            self.w = None  # Discard reference.
 
 
 def main():
