098c0e05f6d118b0d1811ff3eebceb586825c81c
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-05-11 16:30:31 +0100

    Added multithreading to python_qt

--- python-qt/python_qt/app.py
+++ python-qt/python_qt/app.py
@@ -1,22 +1,94 @@
-from PySide6 import QtWidgets
+from typing import Any, Callable
+from PySide6.QtWidgets import (
+    QVBoxLayout,
+    QLabel,
+    QPushButton,
+    QWidget,
+    QMainWindow,
+    QApplication,
+)
+from PySide6.QtCore import QTimer, QRunnable, Slot, QThreadPool
 
 import sys
 
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
+class Worker(QRunnable):
+    """
+    Worker thread
 
-        self.setWindowTitle("Hello World")
-        l = QtWidgets.QLabel("My simple app.")
-        l.setMargin(10)
-        self.setCentralWidget(l)
+    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
+
+    :param callback: The function callback to run on this worker thread. Supplied args and
+                     kwargs will be passed through to the runner.
+    :type callback: function
+    :param args: Arguments to pass to the callback function
+    :param kwargs: Keywords to pass to the callback function
+
+    """
+
+    def __init__(self, fn: Callable[..., Any], *args: int, **kwargs: int):
+        super(Worker, self).__init__()
+        # Store constructor arguments (re-used for processing)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+    @Slot()  # QtCore.Slot
+    def run(self):
+        """
+        Initialise the runner function with passed args, kwargs.
+        """
+        self.fn(*self.args, **self.kwargs)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.threadpool = QThreadPool()
+        print(
+            "Multithreading with maximum %d threads" % self.threadpool.maxThreadCount()
+        )
+
+        self.counter = 0
+
+        layout = QVBoxLayout()
+
+        self.l = QLabel("Start")
+        b = QPushButton("DANGER!")
+        b.pressed.connect(self.oh_no)
+
+        layout.addWidget(self.l)
+        layout.addWidget(b)
+
+        w = QWidget()
+        w.setLayout(layout)
+
+        self.setCentralWidget(w)
+
         self.show()
+
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.recurring_timer)
+        self.timer.start()
+
+    def execute_this_fn(self):
+        print("Hello")
+
+    def oh_no(self):
+        worker = Worker(self.execute_this_fn)
+        self.threadpool.start(worker)
+
+    def recurring_timer(self):
+        self.counter += 1
+        self.l.setText("Counter: %d" % self.counter)
 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
-    w = MainWindow()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
     app.exec()
 
 
