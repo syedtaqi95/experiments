08b6e3e37964d495c4144be12ff71bf0d04665ce
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-11-23 17:29:21 +0000

    Added @adobe/react-spectrum as local dependency

--- react-spectrum/react-spectrum-next-app/src/app/app.tsx
+++ react-spectrum/react-spectrum-next-app/src/app/app.tsx
@@ -1,12 +1,12 @@
-'use client'
+"use client";
 
-import { defaultTheme, Provider } from "@adobe/react-spectrum"
-import type { ReactNode } from "react"
+import { defaultTheme, Provider } from "@adobe/react-spectrum";
+import type { ReactNode } from "react";
 
-export const App = ({ children }: { children: ReactNode }): JSX.Element => {
+export const App = ({ children }: { children: ReactNode }) => {
   return (
-    <Provider theme={defaultTheme}>
+    <Provider theme={defaultTheme} colorScheme="dark">
       {children}
     </Provider>
-  )
-}
+  );
+};
