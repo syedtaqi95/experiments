a0f20774d77d7fb94cf9aa3c5ee2a4e810ec0fab
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-11-24 11:34:13 +0000

    Modified CustomForm

--- react-spectrum/react-spectrum-next-app/src/app/components/CustomForm.tsx
+++ react-spectrum/react-spectrum-next-app/src/app/components/CustomForm.tsx
@@ -1,20 +1,28 @@
 "use client";
 
-import { Button } from "@adobe/react-spectrum";
-import React from "react";
+import { Form, TextField, ButtonGroup, Button } from "@adobe/react-spectrum";
+import { useState } from "react";
 
 const CustomForm = () => {
+  const [name, setName] = useState("");
+
+  const onSubmit: (event: React.FormEvent<HTMLFormElement>) => void = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <Button
-        variant="accent"
-        onPress={() => {
-          console.log("clicked");
-        }}
-      >
-        Save
-      </Button>
-    </div>
+    <Form onSubmit={onSubmit} maxWidth="size-3000">
+      <TextField label="Name" value={name} onChange={setName} />
+      <div>You entered: {name}</div>
+      <ButtonGroup>
+        <Button type="submit" variant="accent">
+          Submit
+        </Button>
+        <Button type="reset" variant="secondary" onPress={() => setName("")}>
+          Reset
+        </Button>
+      </ButtonGroup>
+    </Form>
   );
 };
 
