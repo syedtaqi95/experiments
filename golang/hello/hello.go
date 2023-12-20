774efe7738f6b046e1de1218e6a3683bfb8837b1
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-12-20 23:37:08 +0000

    Added error handling

--- golang/hello/hello.go
+++ golang/hello/hello.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Alice")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, error := greetings.Hello("")
+	if error != nil {
+		log.Fatal(error)
+	}
 	fmt.Println(message)
 }
