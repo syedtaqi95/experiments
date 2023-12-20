898f3f51e95681087e81c20418df205229321ef7
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-12-20 23:58:44 +0000

    Added tests for greetings

--- golang/greetings/greetings.go
+++ golang/greetings/greetings.go
@@ -12,7 +12,8 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in the message.
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprint(randomFormat())
 	return message, nil
 }
 
