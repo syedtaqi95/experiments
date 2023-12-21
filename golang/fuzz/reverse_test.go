22df507cb6e446a1a24fc9eda3ba65508cdba009
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-12-21 23:52:11 +0000

    Completed fuzzing tutorial

--- golang/fuzz/reverse_test.go
+++ golang/fuzz/reverse_test.go
@@ -15,7 +15,10 @@ func TestReverse(t *testing.T) {
 	}
 
 	for _, tc := range testcases {
-		rev := Reverse(tc.in)
+		rev, err := Reverse(tc.in)
+		if err != nil {
+			return
+		}
 		if rev != tc.want {
 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
 		}
@@ -29,9 +32,15 @@ func FuzzReverse(f *testing.F) {
 	}
 
 	f.Fuzz(func(t *testing.T, orig string) {
-		rev := Reverse(orig)
-		doubleRev := Reverse(rev)
-		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+		rev, err1 := Reverse(orig)
+		if err1 != nil {
+			return
+		}
+		doubleRev, err2 := Reverse(rev)
+		if err2 != nil {
+			return
+		}
+
 		if orig != doubleRev {
 			t.Errorf("Before: %q, after: %q", orig, doubleRev)
 		}
