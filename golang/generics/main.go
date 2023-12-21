64129eff7944772d46acaf6e60455a1e034dacdb
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-12-21 23:09:14 +0000

    Completed generics tutorial

--- golang/generics/main.go
+++ golang/generics/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 func main() {
 	ints := map[string]int64{
 		"first": 34, "second": 12,
@@ -13,6 +17,13 @@ func main() {
 
 	fmt.Printf("Non-Generic Sums: %v and %v\n",
 		SumInts(ints), SumFloats(floats))
+
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats(ints), SumIntsOrFloats(floats))
+
+	fmt.Printf("Generic Sums With Constraint: %v and %v\n",
+		SumNumbers(ints), SumNumbers(floats))
+
 }
 
 func SumInts(m map[string]int64) int64 {
@@ -25,6 +36,22 @@ func SumInts(m map[string]int64) int64 {
 
 func SumFloats(m map[string]float64) float64 {
 	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
 	for _, v := range m {
 		s += v
 	}
