396ca4ebf25a6d7fb3192226ea5b9d154d4695ef
syedtaqi95 <sh13226@my.bristol.ac.uk>
2023-12-21 23:42:00 +0000

    Added fuzz test and golang/.gitignore

--- golang/fuzz/reverse_test.go
+++ golang/fuzz/reverse_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"unicode/utf8"
 )
 
 func TestReverse(t *testing.T) {
@@ -19,4 +20,23 @@ func TestReverse(t *testing.T) {
 			t.Errorf("Reverse: %q, want %q", rev, tc.want)
 		}
 	}
+}
+
+func FuzzReverse(f *testing.F) {
+	testcases := []string{"Hello, world", " ", "!12345"}
+	for _, tc := range testcases {
+		f.Add(tc)
+	}
+
+	f.Fuzz(func(t *testing.T, orig string) {
+		rev := Reverse(orig)
+		doubleRev := Reverse(rev)
+		t.Logf("Number of runes: orig=%d, rev=%d, doubleRev=%d", utf8.RuneCountInString(orig), utf8.RuneCountInString(rev), utf8.RuneCountInString(doubleRev))
+		if orig != doubleRev {
+			t.Errorf("Before: %q, after: %q", orig, doubleRev)
+		}
+		if utf8.ValidString(orig) && !utf8.ValidString(rev) {
+			t.Errorf("Reverse produced invalid UTF-8 string %q", rev)
+		}
+	})
 }
