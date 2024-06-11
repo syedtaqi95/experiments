20271f06484628cfdbe4b8668ec34db1c1cad7c0
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-06-11 17:10:22 +0100

    Created custom operator lowercase()

--- rxpy-demo/rxpy_demo/main.py
+++ rxpy-demo/rxpy_demo/main.py
@@ -1,11 +1,43 @@
-from reactivex import of
-from reactivex import operators as op
+from __future__ import annotations
+
+from typing import Callable
+
+import reactivex as rx
+import reactivex.abc as rx_abc
+from reactivex import operators as ops
+
+
+def length_more_than_5() -> Callable[[rx.Observable[str]], rx.Observable[int]]:
+    return rx.compose(
+        ops.map(lambda s: len(s)),
+        ops.filter(lambda i: i >= 5),  # noqa: PLR2004
+    )
+
+
+def lowercase() -> Callable[[rx.Observable[str]], rx.Observable[str]]:
+    def _lowercase(source: rx.Observable[str]) -> rx.Observable[str]:
+        def subscribe(
+            observer: rx_abc.ObserverBase[str],
+            scheduler: rx_abc.SchedulerBase | None = None,
+        ) -> rx_abc.DisposableBase:
+            def on_next(value: str) -> None:
+                observer.on_next(value.lower())
+
+            return source.subscribe(
+                on_next,
+                observer.on_error,
+                observer.on_completed,
+                scheduler=scheduler,
+            )
+
+        return rx.create(subscribe)
+
+    return _lowercase
 
 
 def main() -> None:
-    of("Alpha", "Beta", "Gamma", "Delta", "Epsilon").pipe(
-        op.map(lambda s: len(s)),
-        op.filter(lambda i: i >= 5),  # noqa: PLR2004
+    rx.of("Alpha", "Beta", "Gamma", "Delta", "Epsilon").pipe(
+        lowercase(),
     ).subscribe(lambda value: print(f"Received {value}"))
 
 
