5dae5d15c5d78bbff1d4e4e0214e573f249eb2fd
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-06-12 10:41:29 +0100

    rxpy: added cpu_concurrency()

--- rxpy-demo/rxpy_demo/main.py
+++ rxpy-demo/rxpy_demo/main.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
-from typing import Callable
+import multiprocessing
+import random
+import time
+from threading import current_thread
+from typing import Any, Callable
 
 import reactivex as rx
 import reactivex.abc as rx_abc
 from reactivex import operators as ops
+from reactivex.scheduler import ThreadPoolScheduler
 
 
 # Chaining operators
@@ -49,9 +54,53 @@ def custom_operator() -> None:
     ).subscribe(lambda value: print(f"Received {value}"))
 
 
+# CPU Concurrency
+def intense_calculation(value: Any) -> None:  # noqa: ANN401
+    time.sleep(random.randint(5, 20) * 0.1)  # noqa: S311
+    return value
+
+
+def cpu_concurrency() -> None:
+    optimal_thread_count = multiprocessing.cpu_count()
+    pool_scheduler = ThreadPoolScheduler(optimal_thread_count)
+
+    # Create process 1
+    rx.of("Alpha", "Beta", "Gamma", "Delta", "Epsilon").pipe(
+        ops.map(lambda s: intense_calculation(s)),
+        ops.subscribe_on(pool_scheduler),
+    ).subscribe(
+        on_next=lambda s: print(f"PROCESS 1: {current_thread().name} {s}"),
+        on_error=lambda e: print(e),
+        on_completed=lambda: print("PROCESS 1 done"),
+    )
+
+    # Create process 2
+    rx.range(1, 10).pipe(
+        ops.map(lambda s: intense_calculation(s)),
+        ops.subscribe_on(pool_scheduler),
+    ).subscribe(
+        on_next=lambda s: print(f"PROCESS 2: {current_thread().name} {s}"),
+        on_error=lambda e: print(e),
+        on_completed=lambda: print("PROCESS 2 done"),
+    )
+
+    # Create process 3, which is infinite
+    rx.interval(1).pipe(
+        ops.map(lambda i: i * 100),
+        ops.observe_on(pool_scheduler),
+        ops.map(lambda s: intense_calculation(s)),
+    ).subscribe(
+        on_next=lambda i: print(f"PROCESS 3: {current_thread().name} {i}"),
+        on_error=lambda e: print(e),
+    )
+
+    input("Press Enter key to exit\n")
+
+
 def main() -> None:
     # chaining_operators()
-    custom_operator()
+    # custom_operator()
+    cpu_concurrency()
 
 
 if __name__ == "__main__":
