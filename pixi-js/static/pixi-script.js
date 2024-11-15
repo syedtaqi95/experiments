6941062977f2cca04f2cde2b95d3b01e3c6bdb96
syedtaqi95 <sh13226@my.bristol.ac.uk>
2024-11-15 15:43:52 +0000

    feat: complete parents and children section of pixi docs

--- pixi-js/static/pixi-script.js
+++ pixi-js/static/pixi-script.js
@@ -6,16 +6,44 @@ async function main() {
   // Add canvas to HTML
   document.body.appendChild(app.canvas);
 
+  // Add container to center our sprite on the page
+  const container = new PIXI.Container({
+    x: app.screen.width / 2,
+    y: app.screen.height / 2,
+  });
+  app.stage.addChild(container);
+
   // Add sprite to canvas
   await PIXI.Assets.load("sample.png");
-  let sprite = PIXI.Sprite.from("sample.png");
-  app.stage.addChild(sprite);
+
+  const sprites = [];
+  let parent = container;
+  for (let i = 0; i < 3; i++) {
+    let wrapper = new PIXI.Container();
+    let sprite = PIXI.Sprite.from("sample.png");
+    sprite.anchor.set(0.5);
+    wrapper.addChild(sprite);
+    parent.addChild(wrapper);
+    sprites.push(sprite);
+    parent = wrapper;
+  }
 
   // Create update loop and update sprite's X position over time
   let elapsed = 0.0;
   app.ticker.add((ticker) => {
-    elapsed += ticker.deltaTime;
-    sprite.x = 100.0 + Math.cos(elapsed / 50.0) * 100.0;
+    elapsed += ticker.deltaTime / 60;
+    const amount = Math.sin(elapsed);
+    const scale = 1.0 + 0.25 * amount;
+    const alpha = 0.75 * 0.25 * amount;
+    const angle = 40 * amount;
+    const x = 75 * amount;
+    for (let i = 0; i < sprites.length; i++) {
+      const sprite = sprites[i];
+      sprite.scale.set(scale);
+      sprite.alpha = alpha;
+      sprite.angle = angle;
+      sprite.x = x;
+    }
   });
 }
 
